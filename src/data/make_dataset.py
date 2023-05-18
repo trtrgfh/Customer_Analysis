d70ed28a7dc21da35d3377a8b43d1c6925dcb5b2
trtrgfh <73056232+trtrgfh@users.noreply.github.com>
2023-05-18 10:08:01 -0400

    Add files via upload

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,27 +1,18 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
+def split_data(df):
+  X = df.iloc[:, 1:]
+  y = df[["Y"]]
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=42)
+  return X_train, X_test, y_train, y_test
+  
 url = "https://raw.githubusercontent.com/trtrgfh/nQvq9cIZQdwmNjk7/main/data/ACME-HappinessSurvey2020.csv"
 df = pd.read_csv(url)
-df.head()
+X_train, X_test, y_train, y_test = split_data(df)
 
-print(f"df shape: {df.shape}")
-X = df.iloc[:, 1:]
-y = df[["Y"]]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=42)
-print(f"X_train shape: {X_train.shape}")
-print(f"X_test shape: {X_test.shape}")
-print(f"y_train shape: {y_train.shape}")
-print(f"y_test shape: {y_test.shape}")
-
+# Feature Selection
 print("The corrlelation between the features Xi and target Y")
-df.corr().iloc[0]
-
+print(df.corr().iloc[0])
 df2 = df.drop(["X2", "X4"], axis=1)
-# df2 = df.drop(["X2", "X3"], axis=1)
-
-X2 = df2.iloc[:, 1:]
-y2 = df2[["Y"]]
-X_train2, X_test2, y_train2, y_test2 = train_test_split(X2, y2, test_size=0.15, random_state=42)
-print(f"X_train shape: {X_train2.shape}")
-print(f"X_test shape: {X_test2.shape}")
-print(f"y_train shape: {y_train2.shape}")
-print(f"y_test shape: {y_test2.shape}")
+X_train2, X_test2, y_train2, y_test2 = split_data(df2)
