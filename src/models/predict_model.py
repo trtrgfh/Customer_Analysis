a0340de213f95e261325c56fd84d2d75c94296d1
trtrgfh <73056232+trtrgfh@users.noreply.github.com>
2023-05-18 10:32:42 -0400

    Update predict_model.py

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,6 +1,5 @@
 import xgboost as xgb
 import pickle
-from sklearn.metrics import precision_score, recall_score, accuracy_score, f1_score
 from make_dataset import *
 
 # Load saved model
@@ -16,33 +15,7 @@ model = pickle.load(open("xgboost_model.pkl", "rb"))
 pred_y = model.predict(X_train)
 pred_y_test = model.predict(X_test)
 
-# Calculate metrics for training set
-print(f"Train_Precision: {precision_score(y_train, pred_y)}")
-print(f"Train_Recall: {recall_score(y_train, pred_y)}")
-print(f"Train_Accuracy: {accuracy_score(y_train, pred_y)}")
-print(f"Train_F1score: {f1_score(y_train, pred_y)}")
-
-# Calculate metrics for testing set
-print(f"Test_Precision: {precision_score(y_test, pred_y_test)}")
-print(f"Test_Recall: {recall_score(y_test, pred_y_test)}")
-print(f"Test_Accuracy: {accuracy_score(y_test, pred_y_test)}")
-print(f"Test_F1score: {f1_score(y_test, pred_y_test)}")
-
 # Get model and predictions after feature selection
 new_model = pickle.load(open("xgboost_new_model.pkl", "rb"))
 pred_y2 = new_model.predict(X_train2)
 pred_y_test2 = new_model.predict(X_test2)
-
-# Calculate metrics for training set
-print("Training")
-print(f"Train_Precision: {precision_score(y_train2, pred_y2)}")
-print(f"Train_Recall: {recall_score(y_train2, pred_y2)}")
-print(f"Train_Accuracy: {accuracy_score(y_train2, pred_y2)}")
-print(f"Train_F1score: {f1_score(y_train2, pred_y2)}")
-
-# Calculate metrics for testing set
-print("Testing")
-print(f"Test_Precision: {precision_score(y_test2, pred_y_test2)}")
-print(f"Test_Recall: {recall_score(y_test2, pred_y_test2)}")
-print(f"Test_Accuracy: {accuracy_score(y_test2, pred_y_test2)}")
-print(f"Test_F1score: {f1_score(y_test2, pred_y_test2)}")
