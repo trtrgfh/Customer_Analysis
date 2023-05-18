8d2d42dd7508070d9aee5e841ad9474aae130a3a
trtrgfh <73056232+trtrgfh@users.noreply.github.com>
2023-05-18 10:08:33 -0400

    Add files via upload

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,19 +1,21 @@
-cm = confusion_matrix(y_train, pred_y)
-disp_cm = ConfusionMatrixDisplay(confusion_matrix=cm)
-disp_cm.plot()
-plt.title("Confusion Matrix for Training set")
-plt.show()
+import matplotlib.pyplot as plt
+from predict_model import *
+from make_dataset import *
+
 # Plot confusion matrix for training
 cm = confusion_matrix(y_train, pred_y)
 disp_cm = ConfusionMatrixDisplay(confusion_matrix=cm)
 disp_cm.plot()
 plt.title("Confusion Matrix for Training set")
+plt.savefig('figure1.png', bbox_inches='tight')
 plt.show()
+
 # Plot confusion matrix for testing
 cm_test = confusion_matrix(y_test, pred_y_test)
 disp_cm_test = ConfusionMatrixDisplay(confusion_matrix=cm_test)
 disp_cm_test.plot()
 plt.title("Confusion Matrix for Testing set")
+plt.savefig('figure2.png', bbox_inches='tight')
 plt.show()
 
 im_features = model.feature_importances_
